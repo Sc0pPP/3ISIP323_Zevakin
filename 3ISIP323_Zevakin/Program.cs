4cecffbd35a7d22ec99201f1c7b680c9bcb59e77
Sc0pPP <Zevakidana08@gmail.com>
2025-09-11 16:05:27 +0300

    написали функцию для ввода массива

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,3 +1,11 @@
 ﻿int size;
-string[] prompt = [];
-int []cost = [];
+
+int[] cost = [];
+Console.WriteLine("Введите колличество товаров");
+size=Convert.ToInt32 (Console.ReadLine());
+string[] prompt = new string[size+1];
+Console.WriteLine("Вводите товары по шаблону (Название услуги или товара; Количество денег)");
+for (int i = 0; i <= size; i++)
+{
+    prompt[i] = Console.ReadLine();
+}
