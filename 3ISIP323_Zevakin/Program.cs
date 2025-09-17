b54477fe9d52af8e50623902114cfcb02b2635c2
Sc0pPP <Zevakidana08@gmail.com>
2025-09-17 17:57:23 +0300

    написал функцию добавления товара

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,4 +1,28 @@
 ﻿//проверка
+List<tovar> tovary = new List<tovar>();
+foreach(tovar t in tovary)
+{
+    
+}
+void add()
+{
+
+    tovar.ids++;
+    Console.WriteLine("Введите название товара");
+    string nname = Console.ReadLine();
+    tovar.name = nname;
+    Console.WriteLine("Введите цену товара");
+    tovar.cost = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите колличесвто товара");
+    tovar.count = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите категорию товара\nwater/snack/chebumany");
+    tovar.category = Console.ReadLine();
+}
+void delete()
+{
+
+}
+
 
 enum category : long
 {
@@ -6,13 +30,18 @@ enum category : long
     snack,
     chebumany
 }
+
+
 class tovar
 {
-    int id=1;
-    string name;
-    int cost;
-    int count;
-    bool presence;
-    string category;
 
+    public static int ids = 1;
+    public static int id;
+    public static string name;
+    public static int cost;
+    public static int count;
+    public static bool presence;
+    public static string category;
+   
 }
+
