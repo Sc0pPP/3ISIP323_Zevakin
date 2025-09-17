538a909eb76d73320901ee6b2d7b500c5a5e132a
Sc0pPP <Zevakidana08@gmail.com>
2025-09-17 17:07:59 +0300

    начали создание класса

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,1 +1,18 @@
-﻿//проверка подключения
+﻿//проверка
+
+enum category : long
+{
+    water,
+    snack,
+    chebumany
+}
+class tovar
+{
+    int id=1;
+    string name;
+    int cost;
+    int count;
+    bool presence;
+    string category;
+
+}
