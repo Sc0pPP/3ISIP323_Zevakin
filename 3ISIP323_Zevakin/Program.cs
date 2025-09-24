e6d844d680b8a866ef5a093b30305919185f4642
Sc0pPP <Zevakidana08@gmail.com>
2025-09-24 11:35:42 +0300

    наисали подсчет предложений

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,4 +1,19 @@
 ﻿Console.WriteLine("Введите текст");
 string txt=Console.ReadLine();
-string[] txt_mass = new txt_mass[];
+string[] txt_mass;
 txt_mass = txt.Split(" ");
+int length=txt_mass.Length;
+Console.WriteLine(length);
+string temp="adf";
+for (int i = 1; i < length; i++)
+{
+    if (txt_mass[i].Length < temp.Length)
+    {
+        temp = txt_mass[i];
+    }
+
+}
+Console.WriteLine(temp);
+string[] txt_mass_point=txt.Split(".");
+int count=txt_mass_point.Length;
+Console.WriteLine(count);
