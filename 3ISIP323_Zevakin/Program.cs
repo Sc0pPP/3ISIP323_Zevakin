d900c051b47fc6a5a933efb64b6a3cf342e59c0c
Sc0pPP <Zevakidana08@gmail.com>
2025-09-24 11:51:54 +0300

    написал подсчет гласных и согласных букв

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите текст");
 string txt=Console.ReadLine();
 string[] txt_mass;
+int glasny = 0;
+int soglasny = 0;
 txt_mass = txt.Split(" ");
 int length=txt_mass.Length;
 Console.WriteLine(length);
@@ -11,9 +13,30 @@ for (int i = 1; i < length; i++)
     {
         temp = txt_mass[i];
     }
-
+    
 }
+
+
 Console.WriteLine(temp);
 string[] txt_mass_point=txt.Split(".");
 int count=txt_mass_point.Length;
 Console.WriteLine(count);
+string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+string consonants = "бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ";
+
+int vowelCount = 0;
+int consonantCount = 0;
+
+foreach (char c in txt)
+{
+    if (vowels.Contains(c))
+    {
+        vowelCount++;
+    }
+    else if (consonants.Contains(c))
+    {
+        consonantCount++;
+    }
+}
+Console.WriteLine($"Гласные: {vowelCount}");
+Console.WriteLine($"Согласные: {consonantCount}");
