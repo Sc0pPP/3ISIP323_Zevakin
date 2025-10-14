e8087f11406ae8d4ffb6aa01ab0d9da2f78d9b77
Sc0pPP <Zevakidana08@gmail.com>
2025-10-14 13:33:18 +0300

    Декомпозиция,прозрел,все поправил,ващ круто все

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,20 +1,51 @@
-﻿public class student
-{
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 
-    public student()
+public class person
+{
+    private static int ids;
+    private int id { get;set }
+    private int phone{ get;set }
+    private string name{ get;set }
+    
+}
+public class prepod:person
+{
+    private static int ids;
+    private int experience { get;set }
+
+    public prepod(int experience,int phone, string name):base(id,phone,name)
     {
+        ids += 1;
+        this.id = ids;
+        this.name = name;
 
     }
+
 }
-public class prepod
+public class student : person
 {
-    public prepod()
+    private static int ids;
+    private int group { get; set }
+    private List<university_course> course { get; set }
+
+    public prepod(int group,int course,int phone,string name) : base(id, phone, name)
     {
+        ids += 1;
+        this.id = ids;
+        this.name_pr = name_pr;
+        this.group_num = group_num;
 
     }
 
 }
-public class university :student,prepod
+public class university_course
 {
+    private static int ids;
+    private int id;
+    private string name_cour { get;set }
+    private string time { get;set }
+    private prepod prepod { get;set }
+    public university_course()
 
 }
