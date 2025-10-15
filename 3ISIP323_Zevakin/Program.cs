9ac5e068e971e1bbb708f4647b337e828c3baf81
Sc0pPP <Zevakidana08@gmail.com>
2025-10-15 16:26:44 +0300

    Декомпозиция,Написал все классы игрока и все классы врагов

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class player
+{
+
+}
+class enemy
+{
+
+}
+
+class goblin : enemy
+{
+
+}
+
+class skelet : enemy
+{
+
+}
+
+class mag : enemy
+{
+
+}
+
+class goblin_boss : goblin
+{
+
+}
+
+class skelet_boss_kova: skelet
+{
+
+}
+class skelet_boss_pest : skelet
+{
+
+}
+
+class magc_boss : mag
+{
+
+}
+
