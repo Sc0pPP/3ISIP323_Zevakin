f47d79e9573c2ebceceee4027b267a2421cff7df
Sc0pPP <Zevakidana08@gmail.com>
2025-10-15 15:17:08 +0300

    сделал все по карсивому и перенес все методы в один класс universuty где и находяться все list с данными

--- 3ISIP323_Zevakin/Program.cs
+++ 3ISIP323_Zevakin/Program.cs
@@ -3,19 +3,113 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
 
-public static class GlobalLists
+public class University
 {
-    public static List<student> spisok_st = new List<student>();
-    public static List<person> people = new List<person>();
-    public static List<prepod> prepods = new List<prepod>();
-    public static List<student> students = new List<student>();
-    public static Dictionary<university_course, List<student>> cour_stud = new Dictionary<university_course, List<student>>();
+    public List<student> spisok_st = new List<student>();
+    public List<person> people = new List<person>();
+    public List<prepod> prepods = new List<prepod>();
+    public List<student> students = new List<student>();
+    public Dictionary<university_course, List<student>> cour_stud = new Dictionary<university_course, List<student>>();
+
+    public void add_Teacher()
+    {
+        Console.WriteLine("Введите имя препода");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите его номер телефона");
+        int phone = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите его опыт работы");
+        int exp = Convert.ToInt32(Console.ReadLine());
+        prepod newPrepod = new prepod(exp, phone, name);
+        prepods.Add(newPrepod);
+        people.Add(newPrepod);
+    }
+
+    public void add_student()
+    {
+        Console.WriteLine("Введите имя студента");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите группу");
+        int group = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите курс");
+        int course = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите телефон");
+        int phone = Convert.ToInt32(Console.ReadLine());
+        student newStudent = new student(group, course, phone, name);
+        students.Add(newStudent);
+        people.Add(newStudent);
+    }
+
+    public void sign_up()
+    {
+        student temp = null;
+        Console.WriteLine("Напишите id");
+        int id = Convert.ToInt32(Console.ReadLine());
+        foreach (student s in students)
+        {
+            if (s.Id == id)
+            {
+                temp = s;
+                break;
+            }
+        }
+
+        if (temp == null)
+        {
+            Console.WriteLine("Студент не найден!");
+            return;
+        }
+
+        Console.Write("Напишите название курса");
+        string temp1 = Console.ReadLine();
+        foreach (var key in cour_stud.Keys)
+        {
+            if (key.NameCour == temp1)
+            {
+                cour_stud[key].Add(temp);
+                Console.WriteLine("Студент записан на курс!");
+                return;
+            }
+        }
+        Console.WriteLine("Курс не найден!");
+    }
+
+    public void add_course()
+    {
+        Console.WriteLine("Введите название курса ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите время  ");
+        string time = Console.ReadLine();
+        Console.WriteLine("Введите id препода  ");
+        int ID = Convert.ToInt32(Console.ReadLine());
+
+        prepod foundPrepod = null;
+        foreach (prepod p in prepods)
+        {
+            if (p.Id == ID)
+            {
+                foundPrepod = p;
+                break;
+            }
+        }
+
+        if (foundPrepod == null)
+        {
+            Console.WriteLine("Преподаватель не найден!");
+            return;
+        }
+
+        university_course newCourse = new university_course(name, time, foundPrepod);
+        cour_stud.Add(newCourse, new List<student>());
+        Console.WriteLine("Курс добавлен!");
+    }
 }
 
 class Program
 {
     static void Main()
     {
+        University inver = new University();
+        
         while (true)
         {
             Console.WriteLine("------------------------------------------------\n" +
@@ -33,26 +127,26 @@ class Program
             switch (ttemp)
             {
                 case 1:
-                    student.add_student();
+                    inver.add_student();
                     break;
                 case 2:
-                    prepod.add();
+                    inver.add_Teacher();
                     break;
                 case 3:
-                    university_course.add();
+                    inver.add_course();
                     break;
                 case 4:
-                    student.sign_up();
+                    inver.sign_up();
                     break;
                 case 5:
-                    foreach (person p in GlobalLists.people)
+                    foreach (person p in inver.people)
                     {
                         p.vivod();
                     }
                     break;
                 case 6:
                     int temp = Convert.ToInt32(Console.ReadLine());
-                    foreach (var pair in GlobalLists.cour_stud)
+                    foreach (var pair in inver.cour_stud)
                     {
                         foreach (student s in pair.Value)
                         {
@@ -64,13 +158,13 @@ class Program
                     }
                     break;
                 case 7:
-                    foreach (prepod k in GlobalLists.prepods)
+                    foreach (prepod k in inver.prepods)
                     {
                         Console.WriteLine(k.Id + " " + k.Name);
                     }
                     break;
                 case 8:
-                    foreach (KeyValuePair<university_course, List<student>> cs in GlobalLists.cour_stud)
+                    foreach (KeyValuePair<university_course, List<student>> cs in inver.cour_stud)
                     {
                         Console.WriteLine(cs.Key.NameCour);
                         foreach (student s in cs.Value)
@@ -115,18 +209,7 @@ public class prepod : person
         this.Experience = experience;
     }
 
-    public static void add()
-    {
-        Console.WriteLine("Введите имя препода");
-        string name = Console.ReadLine();
-        Console.WriteLine("Введите его номер телефона");
-        int phone = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите его опыт работы");
-        int exp = Convert.ToInt32(Console.ReadLine());
-        prepod newPrepod = new prepod(exp, phone, name);
-        GlobalLists.prepods.Add(newPrepod);
-        GlobalLists.people.Add(newPrepod);
-    }
+    
 }
 
 public class student : person
@@ -139,55 +222,7 @@ public class student : person
         this.Group = group;
         this.Course = course;
     }
-
-    public static void add_student()
-    {
-        Console.WriteLine("Введите имя студента");
-        string name = Console.ReadLine();
-        Console.WriteLine("Введите группу");
-        int group = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите курс");
-        int course = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите телефон");
-        int phone = Convert.ToInt32(Console.ReadLine());
-        student newStudent = new student(group, course, phone, name);
-        GlobalLists.students.Add(newStudent);
-        GlobalLists.people.Add(newStudent);
-    }
-
-    public static void sign_up()
-    {
-        student temp = null;
-        Console.WriteLine("Напишите id");
-        int id = Convert.ToInt32(Console.ReadLine());
-        foreach (student s in GlobalLists.students)
-        {
-            if (s.Id == id)
-            {
-                temp = s;
-                break;
-            }
-        }
-
-        if (temp == null)
-        {
-            Console.WriteLine("Студент не найден!");
-            return;
-        }
-
-        Console.Write("Напишите название курса");
-        string temp1 = Console.ReadLine();
-        foreach (var key in GlobalLists.cour_stud.Keys)
-        {
-            if (key.NameCour == temp1)
-            {
-                GlobalLists.cour_stud[key].Add(temp);
-                Console.WriteLine("Студент записан на курс!");
-                return;
-            }
-        }
-        Console.WriteLine("Курс не найден!");
-    }
+    
 }
 
 public class university_course
@@ -205,35 +240,5 @@ public class university_course
         this.NameCour = name_cour;
         this.Time = time;
         this.Prepod = prepod;
-    }
-
-    public static void add()
-    {
-        Console.WriteLine("Введите название курса ");
-        string name = Console.ReadLine();
-        Console.WriteLine("Введите время  ");
-        string time = Console.ReadLine();
-        Console.WriteLine("Введите id препода  ");
-        int ID = Convert.ToInt32(Console.ReadLine());
-
-        prepod foundPrepod = null;
-        foreach (prepod p in GlobalLists.prepods)
-        {
-            if (p.Id == ID)
-            {
-                foundPrepod = p;
-                break;
-            }
-        }
-
-        if (foundPrepod == null)
-        {
-            Console.WriteLine("Преподаватель не найден!");
-            return;
-        }
-
-        university_course newCourse = new university_course(name, time, foundPrepod);
-        GlobalLists.cour_stud.Add(newCourse, new List<student>());
-        Console.WriteLine("Курс добавлен!");
     }
 }
