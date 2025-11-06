92bcb3ab31df7c462efc5d010d340ebd1d9255a5
Sc0pPP <Zevakidana08@gmail.com>
2025-11-06 13:06:09 +0300

    написал основные и второстепенные функции, и организовал логику в функции main

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,15 +10,20 @@ namespace ConsoleApp2
     internal class Program
     {
         public static List<tovary> tovars = Core.Context.tovary.ToList();
+        public static string temp_login;
+        public static List<users> Lusers = Core.Context.users.ToList();
+        public static List<cart> Carts = Core.Context.cart.ToList();
+        public static List<pvz> Pvz = Core.Context.pvz.ToList();
+        public static List<order> Orders = Core.Context.order.ToList();
 
         public static void registration()
         {
             Console.WriteLine("Для регистрации напишите ваш логин");
-            string login=Console.ReadLine();
+            string login = Console.ReadLine();
             Console.WriteLine("Напишите пароль");
-            string pasword=Console.ReadLine();
+            string pasword = Console.ReadLine();
             Console.WriteLine("Подтвердите ввш пароль и напишите его еще один раз");
-            string examination=Console.ReadLine();
+            string examination = Console.ReadLine();
             if (pasword == examination)
             {
                 users user = new users
@@ -26,67 +31,132 @@ namespace ConsoleApp2
                     Login = login,
                     passwrod = pasword
                 };
-                Core.Context.users.Add(user); 
+                Core.Context.users.Add(user);
                 Core.Context.SaveChanges();
             }
         }
         public static void list_tovar_and_add_to_cart()
         {
-            List<users> Lusers = Core.Context.users.ToList();
-            Console.WriteLine("Войдите в аакаунт,введите ваш логин");
-            string temp_login = Console.ReadLine();
-            Console.WriteLine("Войдите в аакаунт,введите ваш пароль");
-            string temp_pasword = Console.ReadLine();
-            int id_user = Lusers.First(x => x.Login == temp_login).ID;
-            foreach (users user in Lusers)
+
+            Console.WriteLine("Сегодня в наличии такие товары:");
+            foreach (var tovar in tovars)
             {
-                if (user.Login == temp_login)
-                {
-                    if (user.passwrod == temp_pasword)
-                    {
-                        Console.WriteLine("Вы успешно вошли в свою учетную запись");
-                        Console.WriteLine("Сегодня в наличии такие товары:");
-                        foreach (var tovar in tovars)
-                        {
-                            Console.WriteLine($"id:{tovar.ID},{tovar.Name},цена:{tovar.Price},{tovar.Description}");
+                Console.WriteLine($"id:{tovar.ID},{tovar.Name},цена:{tovar.Price},{tovar.Description}");
 
-                        }
-                        Console.WriteLine("Хотите ли вы что то купить?y/n");
-                        string temp = Console.ReadLine();
-                        if (temp == "y")
-                        {
-                            Console.WriteLine("Введите ID товара");
-                            int temp_tovarid=Convert.ToInt32(Console.ReadLine());
-                            cart cart = new cart
-                            {
-                                UsersID=user.ID,
-                                TovarID=temp_tovarid,
-                            };
-                            
-                            Core.Context.cart.Add(cart);
-                            Core.Context.SaveChanges();
-
-                        }
-
-                    }
-                }
-                {
-                    Console.WriteLine("eror");
-                    break;
-                }
             }
+            Console.WriteLine("Хотите ли вы что то купить?y/n");
+            string temp = Console.ReadLine();
+            int id = Lusers.First(u => u.Login == temp_login).ID;
+            if (temp == "y")
+            {
+                Console.WriteLine("Введите ID товара");
+                int temp_tovarid = Convert.ToInt32(Console.ReadLine());
+                cart cart = new cart
+                {
+                    UsersID = id,
+                    TovarID = temp_tovarid,
+                };
+
+                Core.Context.cart.Add(cart);
+                Core.Context.SaveChanges();
+
+            }
+
         }
+
+    
+                   
+                
+            
+        
         public static void viewing_tovar()
         {
+            Console.WriteLine("Сегодня в наличии такие товары:");
+            foreach (var tovar in tovars)
+            {
+                Console.WriteLine($"id:{tovar.ID},{tovar.Name},цена:{tovar.Price},{tovar.Description}");
+
+            }
 
         }
 
         public static void placing_an_order()
         {
-
+            int id = Lusers.First(u=>u.Login==temp_login).ID;
+            List<cart> temp_order = Carts.Where(u => u.UsersID == id).ToList();
+            foreach (var cart in temp_order)
+            {
+                foreach(var tovar in tovars)
+                {
+                    if (tovar.ID == cart.TovarID)
+                    {
+                        Console.WriteLine($"id:{tovar.ID},{tovar.Name},цена:{tovar.Price},{tovar.Description}");
+                    }
+                }
+            }
+            Console.WriteLine(" хотите ли вы заказать все эти товары?y/n");
+            string temp_choice=Console.ReadLine();
+            if (temp_choice == "y")
+            {
+                Console.WriteLine("Выберете пвз");
+                foreach(pvz punkt in Pvz)
+                {
+                    Console.WriteLine($"{punkt.ID},{punkt.Addres}");
+                }
+                Console.WriteLine("Введите ID пвз");
+                int id_pvz=Convert.ToInt32(Console.ReadLine());
+                foreach(cart cart in temp_order)
+                {
+                    if (id == cart.UsersID)
+                    {
+                        order order = new order()
+                        {
+                            UsersID = id,
+                            TovarID = cart.TovarID,
+                            PvzID=id_pvz,
+                            Date= DateTime.Now,
+                        };
+                        Core.Context.order.Add(order);
+                        Core.Context.cart.Remove(cart);
+                        Core.Context.SaveChanges();
+                        Carts.Remove(cart);
+                    }
+                }
+                Console.WriteLine("Заказ оформлен и скоро убдет доставлен");
+            }
         }
         public static void history_of_orders()
         {
+            int tempID= Lusers.First(x => x.Login == temp_login).ID;
+            List<order> order_user = Orders.Where(u => u.UsersID == tempID).ToList();
+            var groupedOrders = order_user
+    .GroupBy(order => new DateTime(
+        order.Date.Year,
+        order.Date.Month,
+        order.Date.Day,
+        order.Date.Hour,
+        order.Date.Minute,
+        0)) // Обнуляем секунды - теперь группируем только до минут
+    .OrderBy(group => group.Key)
+    .ToList();
+
+            foreach (var orderGroup in groupedOrders)
+            {
+                Console.WriteLine($"Заказ от {orderGroup.Key:dd.MM.yyyy HH:mm}");
+                Console.WriteLine("Товары в заказе:");
+
+                foreach (var item in orderGroup)
+                {
+                    foreach (tovary tovar in tovars) {
+                        if (tovar.ID == item.TovarID)
+                        {
+                            Console.WriteLine($"  - {tovar.Name},{tovar.Price}");
+                        }
+                    }
+                }
+               
+            }
+
 
         }
         static void Main(string[] args)
@@ -94,6 +164,10 @@ namespace ConsoleApp2
             Console.WriteLine("Выберете действие");
             while (true)
             {
+                Console.WriteLine("Выберете:" +
+                    "1://просмотр товара" +
+                    "2://регистрация" +
+                    "3://вход в аккаунт");
                 int temp_choice = Convert.ToInt32(Console.ReadLine());
                 switch (temp_choice) {
                     case 1://просмотр товара
@@ -104,9 +178,9 @@ namespace ConsoleApp2
                         break;
 
                     case 3://вход в аккаунт
-                        List<users> Lusers = Core.Context.users.ToList();
+                        
                         Console.WriteLine("Войдите в аакаунт,введите ваш логин");
-                        string temp_login = Console.ReadLine();
+                        temp_login = Console.ReadLine();
                         Console.WriteLine("Войдите в аакаунт,введите ваш пароль");
                         string temp_pasword = Console.ReadLine();
                         int id_user = Lusers.First(x => x.Login == temp_login).ID;
@@ -117,20 +191,34 @@ namespace ConsoleApp2
                                 if (user.passwrod == temp_pasword)
                                 {
                                     Console.WriteLine("Вы успешно вошли в свою учетную запись");
+                                    while (true) {
+                                        Console.WriteLine("Выберете:" +
+                                            "1://список товаров и добавление в корзину" +
+                                            "2://корзина и оформление заказа" +
+                                            "3://просмотр истории заказов с датой");
+                                        int temp = Convert.ToInt32(Console.ReadLine());
+                                        switch (temp)
+                                        {
+                                            case 1://список товаров и добавление в корзину
+                                                list_tovar_and_add_to_cart();
+                                                break;
+                                            case 2://корзина и оформление заказа
+                                                placing_an_order();
+
+                                                break;
+                                            case 3://просмотр истории заказов с датой
+                                                history_of_orders();
+                                                break;
+                                        }
+                                    }
+
 
                                 }
                             }
-                        }
+                        } 
 
-                                    break;
-                    case 4://корзина и оформление заказа
-                        placing_an_order();
-
-                        break;
-                    case 5://просмотр истории заказов с датой
-                        history_of_orders();
-                        break;
-
+                    break;
+                    
                 }
             }
 
