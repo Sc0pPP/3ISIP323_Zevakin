47fec86c9d3a0a07a68f18478ddaf292a3f4a4f1
Sc0pPP <Zevakidana08@gmail.com>
2025-11-25 14:42:21 +0300

    Добавил нового врага 'Слизь"

---
+++ 3ISIP323_Zevakin/Новая папка/MonsterFabric.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP323_Zevakin.Новая_папка
+{
+    internal class MonsterFabric
+    {
+        static Random rnd = new Random();
+
+        public static Enemy CreateMonster (bool isboss = false)
+        {
+            if (isboss)
+            {
+                int type = rnd.Next(maxValue: 4);
+                switch (type)
+                {
+                    case 0: return new GoblinBoss();
+                    case 1: return new SkeletBossKova();
+                    case 2: return new SkeletBossPest();
+                    case 3: return new MagBoss(); 
+                }
+            }
+            else
+            {
+                int type = rnd.Next(maxValue: 4);
+                switch (type)
+                {
+                    
+                   
+                    case 0: return new Goblin();
+                    case 1: return new Skelet();
+                    case 2: return new Mag();
+                    case 3: return new Slime();
+                }
+
+                }
+            return null;
+
+        }
+    }
+}
---
+++ 3ISIP323_Zevakin/Новая папка/Slime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ISIP323_Zevakin.Новая_папка
+{
+    internal class Slime:Enemy
+    {
+        public Slime() : base(15, 3, 4)
+        {
+            Name = "Слизь";
+            
+
+        }
+    }
+}
